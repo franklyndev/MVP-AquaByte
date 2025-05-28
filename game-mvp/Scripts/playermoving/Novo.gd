a8186269cd6db7b82229e3611d561f8fe02e8395
FranklynSantos <franklynsantos451@gmail.com>
2025-05-28 04:53:44 -0300

    entrada da caverna
    colisão para entrada da caverna

--- game-mvp/Scripts/playermoving/Novo.gd
+++ game-mvp/Scripts/playermoving/Novo.gd
@@ -125,5 +125,5 @@ func _on_hurtbox_body_entered(body: Node2D) -> void:
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	if body.is_in_group("player"):
-		get_tree().change_scene_to_file("res://Scenes/caverna.tscn")
+		get_tree().change_scene_to_file("res://Assets definitivos/Cuspidor/caverna.tscn")
 		
